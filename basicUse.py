d4f3e552cb56eb7d5338773085e0b0ec7cfb3ebc
Hiacecommuter <82562175+Hiacecommuter@users.noreply.github.com>
2023-03-27 17:43:59 +1100

    Update basicUse.py

--- basicUse.py
+++ basicUse.py
@@ -31,6 +31,7 @@ pattern10 = r'[A-Za-z02468]{40}[13579\s]{5}'
 
 #11 {x, y}, matches between x and y (inclusive) repetitions of  character, character class, or groups
 pattern11 = r'\d{1,2}[A-Za-z]{3,}[.0123]{0,3}'
+#{0, } is same as *, {1,} is same as +
 
 #12 *, matches zero or more repetitions
 
@@ -38,7 +39,10 @@ pattern11 = r'\d{1,2}[A-Za-z]{3,}[.0123]{0,3}'
 
 #14 $, the $ boundary matcher matches an occurence of a character, character class, or group at end of a line
 
-
+#extra1 ?, makes the preceding token in the regular expression optional. (same as {0, 1})
+#   ( )?, makes several tokens optional
+pattern_e1 = r"colou?r" # colour and color
+pattern_e12 = r"Nov(ember)?" # Nov and November
 
 
 
